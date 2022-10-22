7adc45dfc05c2dd8a621ffff6d342c39d51753a8
shubh <shubh@Shubham>
2022-10-22 17:23:53 +0530

    This is the first Commit.

---
+++ src/p1/Test.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello guys this project is going to be uploaded in eclipse");
+		
+	}
+
+}
